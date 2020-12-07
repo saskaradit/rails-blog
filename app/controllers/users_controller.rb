aa1fc87a34615c14da6ecfaf6eb170fecf4ac6b1
Rad Saskara <saskaraxyz@gmail.com>
2020-12-07 14:01:29 +0700

    adding user signup and profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,40 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Yay! #{@user.username} you are signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have successfully upated your account"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
