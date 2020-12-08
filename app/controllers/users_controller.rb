34acba2d87bc322c590942927d6face1273b156e
Rad Saskara <saskaraxyz@gmail.com>
2020-12-08 10:36:46 +0700

    adding sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show,:edit,:update]
+
   def new
     @user = User.new
-    
   end
 
   def create
     @user = User.new(user_params)
+    session[:user_id] = @user.id
     if @user.save
       flash[:notice] = "Yay! #{@user.username} you are signed up"
       redirect_to articles_path
@@ -15,11 +17,9 @@ class UsersController < ApplicationController
   end 
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have successfully upated your account"
       redirect_to @user
@@ -29,16 +29,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page],per_page:5)
   end
 
   def index
-    @articles = User.paginate(page: params[:page],per_page:5)
+    @users = User.paginate(page: params[:page],per_page:5)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
