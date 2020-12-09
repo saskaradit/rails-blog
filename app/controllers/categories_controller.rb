d979a14f46d6baf8ae07f1122c916eb4c8a2257b
Rad Saskara <saskaraxyz@gmail.com>
2020-12-09 16:34:21 +0700

    categories test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,27 @@
+class CategoriesController < ApplicationController
+    def new
+        @category = Category.new
+    end
+
+    def index
+    end
+
+    def create 
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "category created"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+    end
+
+    private 
+    def category_params
+        params.require(:category).permit(:name)
+    end
+    
+end
