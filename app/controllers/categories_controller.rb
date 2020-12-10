b03b988480244267b0114328e00e460ce6063814
Rad Saskara <saskaraxyz@gmail.com>
2020-12-10 13:15:04 +0700

    adding admin functionality test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
+
     def new
         @category = Category.new
     end
@@ -25,5 +27,11 @@ class CategoriesController < ApplicationController
     def category_params
         params.require(:category).permit(:name)
     end
-    
+   
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "Only admins can access this page"
+            redirect_to categories_path
+        end
+    end
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username:"last",email:"last@last.com",password:"rad", admin:true)
+    sign_in_as(@admin_user)
+  end
+
   test "get new category form and creating the category " do
     get "/categories/new"
     assert_response :success
